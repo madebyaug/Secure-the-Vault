971f87f5c85ce4fbdff33248c3dfe6f265ef9b9d
August Foreman <your_email@example.com>
2025-05-01 23:55:55 -0400

    Q.6 hard due 2 (+) placements

---
+++ index.js
@@ -0,0 +1,23 @@
+// question 2 – 10
+const code1 = 7 + 3;
+
+// question 3 – 40
+const code2 = 100 / 2 - 10;
+
+// question 4 – 39
+const code3 = 39 % 40;
+
+// question 5 - "The vault has been secured. The combination is:"
+const message = "The vault has been secured. The combination is:";
+
+// question 6 – "10-40-39"
+const codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA);
+
+// question 7 – "10-40-39"
+const codeB = `${code1}-${code2}-${code3}`;
+
+//question 8 – Print “message”, “codeA”, and “codeB”
+console.log("message");
+console.log("codeA");
+console.log("codeB");
